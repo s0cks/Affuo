3575d7a6c3d723cb32c0c3dd21c7dfee9a7ec008
s0cks <tasman.vose@gmail.com>
2016-06-11 20:49:49 +0000

    Hmm does this look okay?

--- app/app.js
+++ app/app.js
@@ -17,8 +17,13 @@ var treeData = {
     ]
 };
 
-app.service("javaToAST", [function(){
-
+app.service("GitHubService", ["$http", function($http){
+    return ({
+        _base_url: "https://api.github.com",
+        getRepository: function(user, repo){
+            return $http.get(this._base_url + "/repos/" + user + "/" + repo);
+        }
+    });
 }]);
 
 app.directive("cfg", [function(){
@@ -27,7 +32,7 @@ app.directive("cfg", [function(){
         replace: true,
         scope: false,
         link: function($scope, $elem, $attrs){
-            var width = (parseInt($attrs.width) || document.body.clientWidth),
+            var width = (parseInt($attrs.width) || document.body.clientWidth - 257),
                 height = (parseInt($attrs.height) || document.body.clientHeight);
 
             var tree = d3.layout.tree()
@@ -148,7 +153,16 @@ app.directive("cfg", [function(){
     };
 }]);
 
-app.controller("mainCtrl", ["$scope", "$injector", function($scope, $injector){
-    $scope.parser = $injector.get("javaToAST");
+app.controller("mainCtrl", ["$scope", "GitHubService", function($scope, $github){
     $scope.tree = treeData;
+    $scope.loadRepository = function(url){
+        if(url.startsWith("https://github.com/")) url = url.replace("https://github.com/", "");
+        if(url.startsWith("http://github.com/")) url = url.replace("http://github.com/", "");
+        var user = url.substring(0, url.lastIndexOf("/"));
+        var repository = url.substring(url.lastIndexOf("/") + 1);
+        $github.getRepository(user, repository)
+            .success(function(data){
+                $scope.repository = data;
+            });
+    };
 }]);
