6de0d3a8f273a730d1171a03f0acdc921e98bccc
s0cks <tasman.vose@gmail.com>
2016-06-12 00:42:05 +0000

    Panning

--- app/TrackBall.js
+++ app/TrackBall.js
@@ -32,6 +32,7 @@ THREE.TrackBall = function (obj, dom) {
         ZOOM: 1 << 2,
         PAN: 1 << 3
     };
+    this.state = State.NONE;
 
     var self = this;
     this.rotate = (function () {
@@ -39,7 +40,6 @@ THREE.TrackBall = function (obj, dom) {
         var quaternion = new THREE.Quaternion();
         return (function(){
             var angle = Math.acos(self.rotateStart.dot(self.rotateEnd) / self.rotateStart.length() / self.rotateEnd.length());
-            console.log(angle);
             if (angle) {
                 axis.crossVectors(self.rotateStart, self.rotateEnd).normalize();
                 angle *= self.rotateSpeed;
@@ -74,7 +74,6 @@ THREE.TrackBall = function (obj, dom) {
             projection.copy(self.object.up).setLength(ball.y);
             projection.add(up.copy(self.object.up).cross(self.position).setLength(ball.x));
             projection.add(self.position.setLength(ball.z));
-            console.log(JSON.stringify(projection));
             return projection;
         });
     })();
@@ -82,6 +81,7 @@ THREE.TrackBall = function (obj, dom) {
         self.position.subVectors(self.object.position, self.target);
         self.rotate();
         self.zoom();
+        self.pan();
         self.object.position.addVectors(self.target, self.position);
         self.object.lookAt(self.target);
         if(self.lastPosition.distanceToSquared(self.object.position) > 0){
@@ -114,6 +114,31 @@ THREE.TrackBall = function (obj, dom) {
             self.zoomStart.y += (self.zoomEnd.y - self.zoomStart.y) * self.dampingFactor;
         }
     };
+    this.getMouseOnScreen = function(x, y, vector){
+        return vector.set(
+            (x - self.screen.left) / self.screen.width,
+            (y - self.screen.top) / self.screen.height
+        );
+    };
+    this.pan = (function(){
+        var mouseChange = new THREE.Vector2();
+        var up = new THREE.Vector3();
+        var pan = new THREE.Vector3();
+
+        return (function(){
+            mouseChange.copy(self.panEnd).sub(self.panStart);
+            if(mouseChange.lengthSq()){
+                mouseChange.multiplyScalar(self.position.length() * self.panSpeed);
+                pan.copy(self.position).cross(self.object.up).setLength(mouseChange.x);
+                pan.add(up.copy(self.object.up).setLength(mouseChange.y));
+
+                self.object.position.add(pan);
+                self.target.add(pan);
+
+                self.panStart.add(mouseChange.subVectors(self.panEnd, self.panStart).multiplyScalar(self.dampingFactor));
+            }
+        });
+    })();
 
     var mouseup = function (e) {
         self.state = State.NONE;
@@ -124,13 +149,23 @@ THREE.TrackBall = function (obj, dom) {
     var mousemove = function (e) {
         if (self.state == State.ROTATE) {
             self.getMouseProjectionOnBall(e.pageX, e.pageY, self.rotateEnd);
+        } else if(self.state == State.PAN){
+            self.getMouseOnScreen(e.pageX, e.pageY, self.panEnd);
         }
     };
 
     var mousedown = function (e) {
-        self.state = State.ROTATE;
-        self.getMouseProjectionOnBall(e.pageX, e.pageY, self.rotateStart);
-        self.rotateEnd.copy(self.rotateStart);
+        if(self.state === State.NONE){
+            self.state = State.PAN;
+        }
+
+        if(self.state == State.ROTATE){
+            self.getMouseProjectionOnBall(e.pageX, e.pageY, self.rotateStart);
+            self.rotateEnd.copy(self.rotateStart);
+        } else if(self.state == State.PAN){
+            self.getMouseOnScreen(e.pageX, e.pageY, self.panStart);
+            self.panEnd.copy(self.panStart);
+        }
 
         document.addEventListener("mousemove", mousemove, false);
         document.addEventListener("mouseup", mouseup, false);
@@ -149,9 +184,26 @@ THREE.TrackBall = function (obj, dom) {
         self.zoomStart.y += delta * 0.01;
     };
 
+    var keydown = function(e){
+        window.removeEventListener("keydown", keydown);
+        self.prevState = self.state;
+        if(self.state !== State.NONE){
+            return;
+        } else if(e.shiftKey){
+            self.state = State.ROTATE;
+        }
+    };
+
+    var keyup = function(e){
+        self.state = self.prevState;
+        window.addEventListener("keydown", keydown, false);
+    };
+
     self.dom.addEventListener("mousewheel", mousewheel, false);
     self.dom.addEventListener("DOMMouseScrool", mousewheel, false);
     self.dom.addEventListener("mousedown", mousedown, false);
+    window.addEventListener("keydown", keydown, false);
+    window.addEventListener("keyup", keyup, false);
     self.handleResize();
 };
 
