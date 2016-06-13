f4a6acaa1c25b60e98460f6e626c1fa36e2fde6c
s0cks <tasman.vose@gmail.com>
2016-06-13 05:10:11 +0000

    Got the graph back

---
+++ app/js/Graph.js
@@ -0,0 +1,155 @@
+
+var Graph = function(){
+    this.nodeSet = {};
+    this.nodes = [];
+    this.edges = [];
+};
+
+Graph.prototype.addNode = function(node){
+    if(this.nodeSet[node] == undefined){
+        var n = new Node(node);
+        this.nodeSet[node] = n;
+        this.nodes.push(n);
+        return true;
+    }
+
+    return false;
+};
+
+Graph.prototype.get = function(node){
+    return this.nodeSet[node];
+};
+
+Graph.prototype.addEdge = function(from, to){
+    if(typeof from === "string"){
+        from = this.nodeSet[from];
+    }
+
+    if(typeof to === "string"){
+        to = this.nodeSet[to];
+    }
+
+    if(from.addConnectedTo(to) == true){
+        var edge = new Edge(from, to);
+        this.edges.push(edge);
+        return true;
+    }
+
+    return false;
+};
+
+var Node = function(id){
+    this.id = id;
+    this.nodesTo = [];
+    this.position = {};
+    this.data = {};
+};
+
+Node.prototype.addConnectedTo = function(node){
+    if(this.connectedTo(node) == false){
+        this.nodesTo.push(node);
+        return true;
+    }
+
+    return false;
+};
+
+Node.prototype.connectedTo = function(node){
+    for(var i = 0; i < this.nodesTo.length; i++){
+        var connected = this.nodesTo[i];
+        if(connected.id == node.id){
+            return true;
+        }
+    }
+
+    return false;
+};
+
+var Edge = function(from, to){
+    this.from = from;
+    this.to = to;
+};
+
+var TreeGraph = function(options){
+    this.graph = options.graph;
+    this.nodesCount = options.nodesCount || 20;
+    this.edgesCount = options.edgesCount || 10;
+    this.xSize = options.size || document.body.clientWidth;
+    this.ySize = options.size || document.body.clientHeight;
+
+    var self = this;
+
+    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    renderer.setSize(this.xSize, this.ySize);
+
+    var camera = new THREE.PerspectiveCamera(40, this.xSize / this.ySize, 1, 10000000);
+    camera.position.z = 5000;
+
+    var trackBall = new THREE.TrackBall(camera);
+    trackBall.rotateSpeed = 0.5;
+    trackBall.dampingFactor = 0.3;
+    trackBall.addEventListener("change", render);
+
+    var scene = new THREE.Scene();
+    var geometry = new THREE.CubeGeometry(100, 100, 100);
+    var geometries = [];
+
+    var drawNode = function(n){
+        var drawObject = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x000000, opacity: 0.5 }));
+        var area = 5000;
+        drawObject.position.x = Math.floor(Math.random() * (area + area + 1) - area);
+        drawObject.position.y = Math.floor(Math.random() * (area + area + 1) - area);
+        drawObject.position.z = Math.floor(Math.random() * (area + area + 1) - area);
+        drawObject.id = n.id;
+
+        n.data.drawObject = drawObject;
+        n.position = drawObject.position;
+        scene.add(n.data.drawObject);
+    };
+
+    var drawEdge = function(source, target){
+        var material = new THREE.LineBasicMaterial({ color: 0xF3F3F3, opacity: 1, linewidth: 0.5 });
+
+        var geo = new THREE.Geometry();
+        geo.vertices.push(source.data.drawObject.position);
+        geo.vertices.push(target.data.drawObject.position);
+
+        var line = new THREE.LineSegments(geo, material);
+        line.scale.x = line.scale.y = line.scale.z = 1;
+        line.originalScale = 1;
+
+        geometries.push(geo);
+
+        scene.add(line);
+    };
+
+    var render = function(){
+        for(var i = 0; i < geometries.length; i++){
+            geometries[i].verticesNeedUpdate = true;
+        }
+
+        renderer.render(scene, camera);
+    };
+
+    var animate = function(){
+        requestAnimationFrame(animate);
+        trackBall.update();
+        render();
+    };
+
+    var compileGraph = function(graph){
+        var i;
+        for(i = 0; i < graph.nodes.length; i++){
+            drawNode(graph.nodes[i]);
+        }
+
+        for(i = 0; i < graph.edges.length; i++){
+            var edge = graph.edges[i];
+            drawEdge(edge.from, edge.to);
+        }
+    };
+
+    compileGraph(this.graph);
+    animate();
+    return renderer.domElement;
+};
---
+++ app/js/TrackBall.js
@@ -0,0 +1,209 @@
+var State = {
+    NONE: 1 << 0,
+    ROTATE: 1 << 1,
+    ZOOM: 1 << 2,
+    PAN: 1 << 3
+};
+
+THREE.TrackBall = function (obj, dom) {
+    this.object = obj;
+    this.dom = (dom !== undefined) ? dom : document;
+
+    this.rotateSpeed = 1.0;
+    this.zoomSpeed = 1.2;
+    this.panSpeed = 0.3;
+    this.dampingFactor = 0.2;
+    this.screen = {
+        left: 0,
+        top: 0,
+        width: 0,
+        height: 0
+    };
+
+    this.target = new THREE.Vector3();
+    this.lastPosition = new THREE.Vector3();
+    this.position = new THREE.Vector3();
+
+    this.rotateStart = new THREE.Vector3();
+    this.rotateEnd = new THREE.Vector3();
+
+    this.zoomStart = new THREE.Vector2();
+    this.zoomEnd = new THREE.Vector2();
+
+    this.panStart = new THREE.Vector2();
+    this.panEnd = new THREE.Vector2();
+
+    this.state = State.NONE;
+
+    var self = this;
+    this.rotate = (function () {
+        var axis = new THREE.Vector3();
+        var quaternion = new THREE.Quaternion();
+        return (function(){
+            var angle = Math.acos(self.rotateStart.dot(self.rotateEnd) / self.rotateStart.length() / self.rotateEnd.length());
+            if (angle) {
+                axis.crossVectors(self.rotateStart, self.rotateEnd).normalize();
+                angle *= self.rotateSpeed;
+                quaternion.setFromAxisAngle(axis, -angle);
+                self.position.applyQuaternion(quaternion);
+                self.object.up.applyQuaternion(quaternion);
+                self.rotateEnd.applyQuaternion(quaternion);
+                quaternion.setFromAxisAngle(axis, angle * (self.dampingFactor - 1.0));
+                self.rotateStart.applyQuaternion(quaternion);
+            }
+        });
+    })();
+    this.getMouseProjectionOnBall = (function(){
+        var up = new THREE.Vector3();
+        var ball = new THREE.Vector3();
+
+        return (function(x, y, projection){
+            ball.set(
+                (x - self.screen.width * 0.5 - self.screen.left) / self.screen.width * 0.5,
+                (self.screen.height * 0.5 + self.screen.top - y) / (self.screen.height * 0.5),
+                0.0
+            );
+
+            var length = ball.length();
+            if(length > 1.0){
+                ball.normalize();
+            } else{
+                ball.z = Math.sqrt(1.0 - length * length);
+            }
+
+            self.position.copy(self.object.position).sub(self.target);
+            projection.copy(self.object.up).setLength(ball.y);
+            projection.add(up.copy(self.object.up).cross(self.position).setLength(ball.x));
+            projection.add(self.position.setLength(ball.z));
+            return projection;
+        });
+    })();
+    this.update = function(){
+        self.position.subVectors(self.object.position, self.target);
+        self.rotate();
+        self.zoom();
+        self.pan();
+        self.object.position.addVectors(self.target, self.position);
+        self.object.lookAt(self.target);
+        if(self.lastPosition.distanceToSquared(self.object.position) > 0){
+            self.dispatchEvent({ type: "changed" });
+            self.lastPosition.copy(self.object.position);
+        }
+    };
+    this.handleEvent = function(e){
+        if(typeof this[e.type] == "function"){
+            this[e.type](e);
+        }
+    };
+    this.handleResize = function(){
+        if(self.dom == document){
+            self.screen.left = 0;
+            self.screen.top = 0;
+            self.screen.width = window.innerWidth;
+            self.screen.height = window.innerHeight;
+        } else{
+            self.screen = self.dom.getBoundingClientRect();
+            var d = self.dom.ownerDocument.documentElement;
+            self.screen.left += window.pageXOffset - d.clientLeft;
+            self.screen.top += window.pageYOffset - d.clientTop;
+        }
+    };
+    this.zoom = function(){
+        var factor = 1.0 + (self.zoomEnd.y - self.zoomStart.y) * self.zoomSpeed;
+        if(factor !== 1.0 && factor > 0.0){
+            self.position.multiplyScalar(factor);
+            self.zoomStart.y += (self.zoomEnd.y - self.zoomStart.y) * self.dampingFactor;
+        }
+    };
+    this.getMouseOnScreen = function(x, y, vector){
+        return vector.set(
+            (x - self.screen.left) / self.screen.width,
+            (y - self.screen.top) / self.screen.height
+        );
+    };
+    this.pan = (function(){
+        var mouseChange = new THREE.Vector2();
+        var up = new THREE.Vector3();
+        var pan = new THREE.Vector3();
+
+        return (function(){
+            mouseChange.copy(self.panEnd).sub(self.panStart);
+            if(mouseChange.lengthSq()){
+                mouseChange.multiplyScalar(self.position.length() * self.panSpeed);
+                pan.copy(self.position).cross(self.object.up).setLength(mouseChange.x);
+                pan.add(up.copy(self.object.up).setLength(mouseChange.y));
+
+                self.object.position.add(pan);
+                self.target.add(pan);
+
+                self.panStart.add(mouseChange.subVectors(self.panEnd, self.panStart).multiplyScalar(self.dampingFactor));
+            }
+        });
+    })();
+
+    var mouseup = function (e) {
+        self.state = State.NONE;
+        document.removeEventListener("mousemove", mousemove);
+        document.removeEventListener("mouseup", mouseup);
+    };
+
+    var mousemove = function (e) {
+        if (self.state == State.ROTATE) {
+            self.getMouseProjectionOnBall(e.pageX, e.pageY, self.rotateEnd);
+        } else if(self.state == State.PAN){
+            self.getMouseOnScreen(e.pageX, e.pageY, self.panEnd);
+        }
+    };
+
+    var mousedown = function (e) {
+        if(self.state === State.NONE){
+            self.state = State.PAN;
+        }
+
+        if(self.state == State.ROTATE){
+            self.getMouseProjectionOnBall(e.pageX, e.pageY, self.rotateStart);
+            self.rotateEnd.copy(self.rotateStart);
+        } else if(self.state == State.PAN){
+            self.getMouseOnScreen(e.pageX, e.pageY, self.panStart);
+            self.panEnd.copy(self.panStart);
+        }
+
+        document.addEventListener("mousemove", mousemove, false);
+        document.addEventListener("mouseup", mouseup, false);
+    };
+
+    var mousewheel = function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        var delta = 0;
+        if(e.wheelDelta){
+            delta = e.wheelDelta / 40;
+        } else if(e.detail){
+            delta = -e.detail / 3;
+        }
+        self.zoomStart.y += delta * 0.01;
+    };
+
+    var keydown = function(e){
+        window.removeEventListener("keydown", keydown);
+        self.prevState = self.state;
+        if(self.state === State.NONE && e.shiftKey){
+            self.state = State.ROTATE;
+        }
+    };
+
+    var keyup = function(e){
+        self.state = self.prevState;
+        window.addEventListener("keydown", keydown, false);
+    };
+
+    self.dom.addEventListener("mousewheel", mousewheel, false);
+    self.dom.addEventListener("DOMMouseScroll", mousewheel, false);
+    self.dom.addEventListener("mousedown", mousedown, false);
+    window.addEventListener("keydown", keydown, false);
+    window.addEventListener("keyup", keyup, false);
+    self.handleResize();
+};
+
+THREE.TrackBall.prototype = Object.create(THREE.EventDispatcher.prototype);
